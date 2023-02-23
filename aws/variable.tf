ce2ab1f2fb14e581e418caff99694227d510e403
chanakanissanka <40533683+chanakanissanka@users.noreply.github.com>
2023-02-23 20:56:39 +1300

    Update variable.tf

--- aws/variable.tf
+++ aws/variable.tf
@@ -1,5 +1,5 @@
 variable "tunnel_config" {
-  type = list(object({
+  type = map(object({
     public_ip_address_gw  = string
     tunnel1_inside_cidr   = string
     tunnel2_inside_cidr   = string
@@ -7,21 +7,21 @@ variable "tunnel_config" {
     tunnel2_preshared_key = string
   }))
 
-  default = [
-    {
+  default = {
+    tn1= {
       public_ip_address_gw  = "1.1.1.1"
       tunnel1_inside_cidr   = "169.254.21.0/30"
       tunnel2_inside_cidr   = "169.254.21.4/30"
       tunnel1_preshared_key = "fgdytdgdstsgshasgsgsashh"
       tunnel2_preshared_key = "fgdytdgdstsgshasgsgsashh"
-    },
-    {
+    }
+    tn2 = {
       public_ip_address_gw  = "1.1.1.2"
       tunnel1_inside_cidr   = "169.254.21.28/30"
       tunnel2_inside_cidr   = "169.254.21.32/30"
       tunnel1_preshared_key = "fgdytdgdstsgshasgsgsashh"
       tunnel2_preshared_key = "fgdytdgdstsgshasgsgsashh"
     }
-  ]
+  }
   sensitive = true
 }
